f134e2256e1f54645072a62da45c1f8ff2aa4650
Danilo <danilonaves2209@gmail.com>
2023-06-04 20:25:28 -0300

    Bubble

--- Sort/bubble/bubble_sort.c
+++ Sort/bubble/bubble_sort.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void bubble (int vetor, int l, int r) {
-    
+void bubble(int vetor[], int l, int r)
+{
+    int j = 0;
+    int swap = 1;
+    while (swap == 1)
+    {
+        swap = 0;
+        for (int i = r - 1; i > 0; i--)
+        {
+            if (vetor[i] < vetor[i - 1])
+            {
+                int aux = vetor[i];
+                vetor[i] = vetor[i - 1];
+                vetor[i - 1] = aux;
+                swap = 1;
+            }
+        }
+    }
 }
 
-int main () {
+int main()
+{
+    int vetor[] = {4, 2, 5, 6, 7, 4, 1};
+    bubble(vetor, 0, 7);
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d\n", vetor[i]);
+    }
     return 0;
 }
+
+//Complexidade assintótica
+    // n^2 / 2, melhor caso N(0);
+//Estabilidade
+    //Sim, mantém a ordem, sendo assim, ele é estável
+//Adaptabilidade
+    //No melhor caso sim.
