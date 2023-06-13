e0d982352a578a0d03958d01f8e3b9d48a526b04
Danilo Naves <danilonaves2209@gmail.com>
2023-06-12 21:44:27 -0300

    shellsort

--- Sort/bubble/shell_sort.c
+++ Sort/bubble/shell_sort.c
@@ -10,9 +10,23 @@ void printa_vetor(int vetor[], int TAM)
     printf("FIM\n");
 }
 //--------------------------------------//
-void shell_sort(int vetor[], int l, int r)
+void shell_sort(int v[], int l, int r)
 {
-    
+    int h = 1;
+    while(h < (r-l+1)/3) h = 3*h+1;
+
+    while(h>=1){
+        for(int i=l+h; i<=r; i++)
+        {
+            for(int j=i; j>=l+h && v[j]<v[j-h]; j-=h)
+            {
+                int a = v[j];
+                v[j] = v[j-h];
+                v[j-h] = a;
+            }
+        }
+        h = h/3;
+    }
 }
 
 int main()
